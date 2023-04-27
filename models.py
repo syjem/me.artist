96dd2a2f07995da07d5bdbaa23da66f8db5daad9
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-04-27 16:11:22 +0800

    models own file

---
+++ models.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+
+class Users(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.first_name} {self.last_name}, Email: {self.email}>'
